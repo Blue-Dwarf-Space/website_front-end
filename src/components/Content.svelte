3cd0f9ddffb1b6b28f94a797d0142d600b6a76f7
Kelly Yeoh <1103515+somewoman@users.noreply.github.com>
2022-09-03 09:52:46 +0000

    remake of content.astro, but in svelte

---
+++ src/components/Content.svelte
@@ -0,0 +1,60 @@
+<script>
+    export let header;
+    export let subheader;
+    export let imageLoc;
+    export let imageAlt;
+    export let para1;
+    export let para2;
+    export let para3;
+
+</script>
+
+<section class="content">
+    <div class="content-text">
+        <h1>{header}</h1>
+        <h2>{subheader}</h2>
+        <img class="mx-auto" src={imageLoc} alt={imageAlt} />
+        <p>{para1}</p>
+        <p>{para2}</p>
+        <p>{para3}</p>
+    </div>
+</section>
+
+
+<style>
+    .content {
+    height: auto;
+    padding: 5px;
+  }
+  .content-blog {
+    min-height: 300px;
+  }
+  .content-text {
+    color: #eee;
+    text-align: center;
+    margin: 30px auto;
+  }
+  .content h1 {
+    font-size: 50px;
+    color: #026087;
+    margin-bottom: 10px;
+  }
+  .content h2 {
+    font-size: 28px;
+    color: #00cc66;
+    margin-bottom: 20px;
+  }
+  .content p {
+    font-size: 18px;
+    color: #eeeeee;
+    margin-bottom: 20px;
+    text-align: left;
+  }
+  .content-img {
+    text-align: center;
+  }
+  .content-img img {
+    border-radius: 10px;
+    margin: auto;
+  }
+  </style>
